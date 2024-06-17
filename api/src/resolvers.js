ef4e4518a586ccb5cc36a1998d2ac4ff9fa0042f
PRSMendis <rayhan.mendis@gmail.com>
2024-06-17 16:00:51 +1000

    relationship resolver kinda working

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -7,8 +7,8 @@ module.exports = {
   Query: {
     User: async (_, {input}, { models }) => {
       // Fetch user from database
-      const {id, username} = input
-      const user = await models.User.findOne({ id });
+      // const {id, username} = input
+      const user = await models.User.findOne({ input });
 
       // If no user found, throw an error
       if (!user) {
@@ -55,14 +55,42 @@ module.exports = {
     }
     
   },
-  // Pet: {
-  //   img(pet) {
-  //     return pet.type === 'DOG'
-  //       ? 'https://placedog.net/300/300'
-  //       : 'http://placekitten.com/300/300'
-  //   }
-  // },
+  Pet: {
+    user(_, __, {models, user}) {
+      return user
+
+    }
+    // img(pet) {
+    //   return pet.type === 'DOG'
+    //     ? 'https://placedog.net/300/300'
+    //     : 'http://placekitten.com/300/300'
+    // }
+  },
   User: {
-    
+    async pets(user,_, {models}) {
+      console.log('user: ', user);
+      // const pet = await models.Pet.findOne({ "user.id": user.id });
+      // const pet = await models.Pet.findOne((pet) => pet.user.id === user.id)
+      // const pet = await models.Pet.findOne((pet) => pet.user.id === "46ycTobse249iZDspxvjV")
+
+      const query = {};
+      // if (name) query.name = name;
+      // if (type) query.type = type;
+      query.user = {}
+      query.user.id = user.id
+
+      const pets = await models.Pet.findMany(query)
+      // const pets = await models.Pet.findMany()
+      console.log('pets: ', pets);
+
+
+      if (!pets) {
+        throw new Error('Pet nots found');
+      }
+
+      return pets
+      // return [{id: 5, name: 'wnos'}]
+      
+    }
   }
 }
--- api/src/server.js
+++ api/src/server.js
@@ -5,7 +5,8 @@ const {models, db} = require('./db')
 
 const server = new ApolloServer({
   context() {
-    return {models, db}
+    const user = models.User.findOne()
+    return {models, db, user}
   },
   typeDefs,
   resolvers
